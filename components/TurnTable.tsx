2d0a665d7ea3886e87f0ad762778c60fc18c45c4
Yu,Maple( China-Chengdu ) <mapleyu@aaxischina.com>
2022-06-22 18:40:04 +0800

    add food type

--- components/TurnTable.tsx
+++ components/TurnTable.tsx
@@ -8,60 +8,126 @@ const TurnTable: FC = () => {
     { padding: '10px', background: '#e9e8fe' },
     { padding: '10px', background: '#869cfa' },
   ]);
-  const [prizes] = useState([
-    {
-      background: '#e9e8fe',
-      range: 25,
-      fonts: [{ text: '炸鸡' }],
-      imgs: [
-        {
-          src: 'https://img1.baidu.com/it/u=1537115176,1287674847&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=427',
-          height: '50px',
-          width: '60px',
-          top: '30px',
-        },
-      ],
-    },
-    {
-      background: '#b8c5f2',
-      range: 25,
-      fonts: [{ text: '汉堡' }],
-      imgs: [
-        {
-          src: 'https://img2.baidu.com/it/u=1170719478,4207668301&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
-          height: '50px',
-          width: '60px',
-          top: '30px',
-        },
-      ],
-    },
-    {
-      background: '#e9e8fe',
-      range: 25,
-      fonts: [{ text: '鸡肉卷' }],
-      imgs: [
-        {
-          src: 'https://img1.baidu.com/it/u=2617048388,173066499&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
-          height: '50px',
-          width: '60px',
-          top: '30px',
-        },
-      ],
-    },
-    {
-      background: '#b8c5f2',
-      range: 25,
-      fonts: [{ text: '薯条' }],
-      imgs: [
-        {
-          src: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F022R1101525%2F21022Q01525-9-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1658398577&t=7124e43f9018c247ecc7160a04004fa0',
-          height: '50px',
-          width: '60px',
-          top: '30px',
-        },
-      ],
-    },
-  ]);
+
+  const foodType = {
+    中式快餐: [
+      {
+        text: '炒饭',
+        img: 'https://img2.baidu.com/it/u=1101523207,651871526&fm=253&fmt=auto&app=138&f=JPEG?w=497&h=294',
+      },
+      {
+        text: '炒面',
+        img: 'https://img0.baidu.com/it/u=2609875662,1070264121&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+      },
+      {
+        text: '炒河粉',
+        img: 'https://img1.baidu.com/it/u=411924035,2701001621&fm=253&fmt=auto&app=138&f=JPG?w=669&h=500',
+      },
+      {
+        text: '盖浇饭',
+        img: 'https://img1.baidu.com/it/u=2816415124,413253130&fm=253&fmt=auto&app=138&f=JPEG?w=735&h=500',
+      },
+    ],
+    面食: [
+      { text: '抄手' },
+      { text: '面条' },
+      { text: '米粉' },
+      { text: '饺子' },
+    ],
+    外国快餐: [
+      {
+        text: '炸鸡',
+        img: 'https://img1.baidu.com/it/u=1537115176,1287674847&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=427',
+      },
+      {
+        text: '汉堡',
+        img: 'https://img2.baidu.com/it/u=1170719478,4207668301&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+      },
+      {
+        text: '鸡肉卷',
+        img: 'https://img1.baidu.com/it/u=2617048388,173066499&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+      },
+      {
+        text: '薯条',
+        img: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F022R1101525%2F21022Q01525-9-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1658398577&t=7124e43f9018c247ecc7160a04004fa0',
+      },
+    ],
+  };
+
+  //   const prizes = [
+  //     {
+  //       background: '#e9e8fe',
+  //       range: 25,
+  //       fonts: [{ text: '炸鸡' }],
+  //       imgs: [
+  //         {
+  //           src: 'https://img1.baidu.com/it/u=1537115176,1287674847&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=427',
+  //           height: '50px',
+  //           width: '60px',
+  //           top: '30px',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       background: '#b8c5f2',
+  //       range: 25,
+  //       fonts: [{ text: '汉堡' }],
+  //       imgs: [
+  //         {
+  //           src: 'https://img2.baidu.com/it/u=1170719478,4207668301&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+  //           height: '50px',
+  //           width: '60px',
+  //           top: '30px',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       background: '#e9e8fe',
+  //       range: 25,
+  //       fonts: [{ text: '鸡肉卷' }],
+  //       imgs: [
+  //         {
+  //           src: 'https://img1.baidu.com/it/u=2617048388,173066499&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+  //           height: '50px',
+  //           width: '60px',
+  //           top: '30px',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       background: '#b8c5f2',
+  //       range: 25,
+  //       fonts: [{ text: '薯条' }],
+  //       imgs: [
+  //         {
+  //           src: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F022R1101525%2F21022Q01525-9-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1658398577&t=7124e43f9018c247ecc7160a04004fa0',
+  //           height: '50px',
+  //           width: '60px',
+  //           top: '30px',
+  //         },
+  //       ],
+  //     },
+  //   ];
+
+  const [prizes] = useState(
+    foodType.中式快餐.map((item, index) => {
+      const newItem = {
+        background: index % 2 ? '#e9e8fe' : '#b8c5f2',
+        range: 25,
+        fonts: [{ text: item.text }],
+        imgs: [
+          {
+            src: item.img,
+            height: '50px',
+            width: '60px',
+            top: '30px',
+          },
+        ],
+      };
+      return newItem;
+    })
+  );
+
   const [buttons] = useState([
     { radius: '40%', background: '#617df2' },
     { radius: '35%', background: '#afc8ff' },
